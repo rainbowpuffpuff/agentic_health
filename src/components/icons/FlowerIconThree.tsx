4d79b531ec904ac5ee5c06a43e9462e20d2e9fc2
Stuff <stufflaters@gmail.com>
2025-08-25 14:51:50 +0000

    in "my action garden" draw several unique flowers in gray overlay with o

---
+++ src/components/icons/FlowerIconThree.tsx
@@ -0,0 +1,27 @@
+
+import type { SVGProps } from "react";
+
+const FlowerIconThree = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth={1}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+    >
+        <path d="M12 5.5A2.5 2.5 0 0 0 9.5 8h5A2.5 2.5 0 0 0 12 5.5z" />
+        <path d="M16 9.5a2.5 2.5 0 0 0 2.5 2.5v-5A2.5 2.5 0 0 0 16 9.5z" />
+        <path d="M14.5 16a2.5 2.5 0 0 0 -2.5 2.5h5A2.5 2.5 0 0 0 14.5 16z" />
+        <path d="M8 14.5a2.5 2.5 0 0 0 -2.5 -2.5v5A2.5 2.5 0 0 0 8 14.5z" />
+        <path d="M9.5 8a2.5 2.5 0 0 0 -2.5 2.5v-5A2.5 2.5 0 0 0 9.5 8z" />
+        <path d="M18.5 12a2.5 2.5 0 0 0 -2.5 -2.5h5A2.5 2.5 0 0 0 18.5 12z" />
+        <path d="M12 18.5a2.5 2.5 0 0 0 2.5 -2.5h-5A2.5 2.5 0 0 0 12 18.5z" />
+        <path d="M5.5 12a2.5 2.5 0 0 0 2.5 2.5h-5A2.5 2.5 0 0 0 5.5 12z" />
+        <circle cx="12" cy="12" r="2.5" fill="white" />
+    </svg>
+);
+
+export default FlowerIconThree;
